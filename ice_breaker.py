1e5a6092886f243cd7e39b855846364ed12a1bc8
Pool Golez <loop.edward@gmail.com>
2024-09-02 13:03:28 +0800

    Change template to use information from repository

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,21 +2,23 @@ from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama import ChatOllama
 
+from third_parties.github_api import list_repositories
+
 if __name__ == '__main__':
-    information = """
-    My name is Ruiz. I killed my father, Carlos. I have a son named Lucas.
-    """
-
     prompt_template = """
-    Given the information {information}, who killed Carlos?
+    Given the following Github repositories {information} of a software engineer, provide the following:
+    1. a short summary of the engineer
+    2. the top list of programming languages used
     """
-
     summary_prompt_template = PromptTemplate(template=prompt_template, input_variables=["information"])
+    username = "poolgolez"
 
     llm = ChatOllama(model="llama3")
     chain = summary_prompt_template | llm | StrOutputParser()
-    print("Invoking chain....")
-    res = chain.invoke(input={"information": information})
+    repos = list_repositories(username, limit=50, mock=True)
 
+    print("The model is thinking...")
+    repository_summary = "\n".join([f"* {str(repo)}" for repo in repos])
+    res = chain.invoke(input={"information": repository_summary})
     print(res)
     print("Done.")
