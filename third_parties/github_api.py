f24489ee46bacb7b6f7ebe96be53ebaec31635a9
Pool Golez <loop.edward@gmail.com>
2024-09-02 14:22:05 +0800

    Add method to search user based on full name

--- third_parties/github_api.py
+++ third_parties/github_api.py
@@ -5,6 +5,18 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+API_KEY = os.getenv('GITHUB_API_KEY')
+
+
+def search_user(name: str):
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {API_KEY}"
+    }
+    params = {"q": name}
+    response = requests.get(f"https://api.github.com/search/users", params=params, headers=headers)
+    users = [User(**user, username=user["login"]) for user in (json.loads(response.text)["items"])]
+    return users[0]
 
 
 def list_repositories(username: str, limit: int = 8, mock: bool = False):
@@ -13,24 +25,25 @@ def list_repositories(username: str, limit: int = 8, mock: bool = False):
         file = open(file_path, "r")
         content = file.read()
         file.close()
-        repos = [GithubRepo(**repo) for repo in json.loads(content)]
+        repos = [Repository(**repo) for repo in json.loads(content)]
     else:
         headers = {
             "Accept": "application/vnd.github+json",
-            "Authorization": f"Bearer {os.getenv('GITHUB_API_KEY')}"
+            "Authorization": f"Bearer {API_KEY}"
         }
         params = {
             "sort": "updated",
             "type": "all"
         }
         response = requests.get(f"https://api.github.com/users/{username}/repos", params=params, headers=headers)
-        repos = [GithubRepo(**repo) for repo in json.loads(response.text)]
+        repos = [Repository(**repo) for repo in json.loads(response.text)]
 
+    print(f"There are {len(repos)} repositories.")
     repo_count = min(limit, len(repos))
     return repos[:repo_count]
 
 
-class GithubRepo:
+class Repository:
     def __init__(self, **kwargs):
         self.id = kwargs['id']
         self.name = kwargs['name']
@@ -47,6 +60,10 @@ class GithubRepo:
     def __repr__(self):
         return str(self)
 
+class User:
+    def __init__(self, **kwargs):
+        self.id = kwargs["id"]
+        self.username = kwargs["username"]
 
 if __name__ == '__main__':
     repos = list_repositories("poolgolez", mock=True)
